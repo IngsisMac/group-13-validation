904bb0461e31749f3a6717ff1f2816c956f02baf
MackyAlimena <malimena@mail.austral.edu.ar>
2026-08-23 17:50:46 -0300

    feat(interpreter): implement PrintScriptInterpreter adapter

--- src/main/java/implementation/CustomImplementationFactory.java
+++ src/main/java/implementation/CustomImplementationFactory.java
@@ -1,11 +1,17 @@
 package implementation;
 
+import com.printscript.common.EnvSource;
+import com.printscript.common.InputSource;
+import com.printscript.common.OutputEmitter;
+import com.printscript.common.PrintScriptError;
 import com.printscript.common.Version;
+import com.printscript.runner.ExecutionResult;
 import com.printscript.runner.PrintScriptRunner;
 import interpreter.PrintScriptFormatter;
 import interpreter.PrintScriptInterpreter;
 import interpreter.PrintScriptLinter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -15,9 +21,36 @@ public class CustomImplementationFactory implements PrintScriptFactory {
 
     @Override
     public PrintScriptInterpreter interpreter() {
-        // your PrintScript implementation should be returned here.
-        // make sure to ADAPT your implementation to PrintScriptInterpreter interface.
-        throw new NotImplementedException("Needs implementation"); // TODO: implement
+        return (src, versionStr, emitter, handler, provider) -> {
+            try {
+                Version version = resolveVersion(versionStr);
+                if (version == null) {
+                    handler.reportError("Unknown version: " + versionStr);
+                    return;
+                }
+                try (Reader reader = new BufferedReader(new InputStreamReader(src, StandardCharsets.UTF_8), 768 * 1024)) {
+                    OutputEmitter outputEmitter = emitter::print;
+                    InputSource inputSource = provider::input;
+                    EnvSource envSource = System::getenv;
+
+                    ExecutionResult result = PrintScriptRunner.INSTANCE.execute(
+                        reader,
+                        version,
+                        outputEmitter,
+                        inputSource,
+                        envSource
+                    );
+
+                    for (PrintScriptError error : result.getErrors()) {
+                        handler.reportError(error.render());
+                    }
+                } catch (IOException e) {
+                    handler.reportError("IO Error: " + e.getMessage());
+                }
+            } catch (OutOfMemoryError e) {
+                handler.reportError("Java heap space");
+            }
+        };
     }
 
     @Override
